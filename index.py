9240c7cb1a10932e49bde7cb38b4a43cf1dd9c95
Unknown <niklas-schunk@t-online.de>
2018-06-18 13:44:40 +0200

    add bootstrap and jquery

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 #!/Library/Frameworks/Python.framework/Versions/3.5/bin/python3.5
 
-
-from tornado import template, ioloop, web
+import os
+from tornado import template, ioloop, web, httpserver
 
 class WebManager:
     def __init__(self):
@@ -13,13 +13,16 @@ class WebManager:
 
 class MainHandler(web.RequestHandler):
     def get(self):
-        loader = template.Loader('./templates')
-        self.write(loader.load('home.html').generate(myvalue='hallo welt'))
+        #loader = template.Loader('./templates')
+        #self.write(loader.load('home.html').generate(myvalue='hallo welt',))
+        self.render('home.html', myvalue="Hello, world!")
 
 
 if __name__ == "__main__":
-    application = web.Application([
-        (r"/", MainHandler),
-    ])
-    application.listen(8888)
-    ioloop.IOLoop.current().start()
+    app = web.Application( handlers=[
+        (r'/', MainHandler)],
+        static_path=os.path.join(os.path.dirname(__file__), "static"),
+        template_path=os.path.join(os.path.dirname(__file__), "templates"))
+    http_server = httpserver.HTTPServer(app)
+    http_server.listen(8888)
+    ioloop.IOLoop.instance().start()
